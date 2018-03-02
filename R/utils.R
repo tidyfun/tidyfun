5c0b6167cb4013735adbf03ca88e7b6ab93895b9
fabian-s <fabian.scheipl@googlemail.com>
2018-03-02 14:39:34 +0100

    document rgp

--- R/utils.R
+++ R/utils.R
@@ -173,11 +173,30 @@ summarize_fvector <- function(..., op = NULL, eval = FALSE) {
   }
 }
 
+#' Random generator for Gaussian Processes
+#' 
+#' The function generates `n` realizations of Gaussian processes , either with
+#' squared exponential covariance \eqn{Cov(x(t), x(t')) = \exp(-(t'-t)^2)/s) + n
+#' \delta_{t}(t')} or Wiener process covariance \eqn{Cov(x(t), x(t')) =
+#' \min(t',t)/s + n \delta_{t}(t')} with `scale` parameter s and `nugget` effect
+#' n.
+#'
+#' @param n how many realizations to draw
+#' @param grid vector of evaluation points (`argvals` of the return object).
+#'   Defaults to (0, 0.02, 0.04, ..., 1).
+#' @param scale scale parameter (see Description). Defaults to the width of the
+#'   domain divided by 10.
+#' @param cor type of correlation structure to use. Currently available:
+#'   "squareexp" or "wiener".
+#' @param nugget nugget effect for additional white error noise. Defaults to
+#'   `scale/200`
+#' @return an `feval`-vector of length `n`
 #' @importFrom mvtnorm rmvnorm
+#' @export
 rgp <- function(n, grid = seq(0, 1, l = 51), scale = diff(range(grid))/10, 
   cor = c("squareexp", "wiener"), nugget = scale/200) {
   cor <- match.arg(cor)
-  f_cov <- switch(cor, "wiener" = function(s, t) scale * pmin(s, t),
+  f_cov <- switch(cor, "wiener" = function(s, t) pmin(s, t)/scale,
     "squareexp" = function(s,t) exp(-(s - t)^2/scale))
   cov <- outer(grid, grid, f_cov) + diag(0*grid + nugget)
   y <- rmvnorm(n, mean = 0 * grid, sigma = cov)
