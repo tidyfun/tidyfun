854b8a460b31f1c26ef2cbd912a98e97c6f92ce2
Fabian Scheipl <fabian.scheipl@googlemail.com>
2018-07-19 17:57:51 +0200

    implement "where". closes #22, kindof at least.

---
+++ R/where.R
@@ -0,0 +1,99 @@
+#' Find out where functional data fulfills certain conditions.
+#'
+#' This function allows to define a logical expression about the function values
+#' and returns the argument values for which that condition is true.
+#'
+#' Entries in `f` that do not fulfill `cond` anywhere yield `numeric(0)`.\cr
+#' `cond`  is evaluated as a [dplyr::filter()]-statement on a `data.frame`
+#' containing a single entry in `f` with columns `arg` and `value`, so all
+#' the usual `dplyr` tricks are available, see examples.\cr
+#' Any condition evaluates to `NA` on `NA`-entries in `f`.
+#'
+#' @param f a `tf` object
+#' @param cond a logical expression on `value` that defines the condition about
+#'   the function values, see examples and details.
+#' @param return for each entry in `f`, `where` either returns *all* `arg` for
+#'   which `cond` is true, the *first*, the *last* or their *range* or logical
+#'   flags whether the functions fullfill the condition *any*where. For
+#'   `"range"`, note that `cond` may not be true for all `arg` values in this
+#'   range, though, this is not checked.
+#' @param arg optional `arg`-values on which to evaluate `f` and check `cond`,
+#'   defaults to `arg(f)`.
+#' @return `return = "any"` returns a logical vector, the others a list of
+#'   numeric a vectors of the same length as `f`.
+#' @examples 
+#'   lin <- 1:4 * tfd(seq(-1, 1,l = 11), seq(-1, 1, l = 11))
+#'   where(lin, value %inr% c(-1, .5))
+#'   where(lin, value %inr% c(-1, .5), "range")
+#'   a <- 1
+#'   where(lin, value > a, "first")
+#'   where(lin, value < a, "last")
+#'   where(lin, value > 2, "any")
+#' 
+#'   set.seed(4353)
+#'   plot(f <- rgp(5, 11L), pch = as.character(1:5), points = TRUE)
+#'   where(f, value == max(value))
+#'   # where is the function increasing/decreasing:
+#'   where(f, value > dplyr::lag(value, 1, value[1]))
+#'   where(f, value < dplyr::lead(value, 1, value[n()]))
+#'   # where are the (interior) extreme points:
+#'   where(f, 
+#'     sign(c(diff(value)[1], diff(value))) !=
+#'       sign(c(diff(value), diff(value)[n()-1])))
+#'   # where for arg > .5 is the function positive:
+#'   where(f, arg > .5 & value > 0)
+#' @export   
+where <- function(f, cond, return = c("all", "first", "last", "range", "any"),
+    arg) {
+  if (missing(arg)) {
+    arg <- tidyfun::arg(f)
+  } else assert_arg(arg, f)
+  return <- match.arg(return)
+  cond_q <- enquo(cond)
+  where_at <- map(f[, arg, matrix = FALSE], 
+    ~ filter(.x, !! cond_q) %>% pull(arg))
+  if (return == "first") {
+    where_at <- map_if(where_at, ~ length(.x) > 0, min)
+  }
+  if (return == "last") {
+    where_at <- map_if(where_at, ~ length(.x) > 0, max)
+  }
+  if (return == "range") {
+    where_at <- map_if(where_at, ~ length(.x) > 0, range)
+  }
+  if (return == "any") {
+    where_at <- map_lgl(where_at, ~ length(.x) > 0)
+  }
+  where_at[is.na(f)] <- NA
+  where_at
+}
+#' @description `in_range` and its infix-equivalent return `TRUE` for all 
+#'    values in `f` that are within the range of values in `r`. 
+#' @param r used to specify a range, only the minimum and maximum of `r` are used.
+#' @rdname where
+#' @export
+in_range <- function(f, r){
+  assert_numeric(f)
+  assert_numeric(r)
+  r <- range(r, na.rm = TRUE)
+  f >= r[1] & f <= r[2]
+}
+#' @rdname where
+#' @export
+`%inr%` <- function(f, r) in_range(f, r)
+
+if (FALSE) {
+  lin <- 1:4 * tfd(seq(-1, 1,l = 11), seq(-1, 1, l = 11))
+  where(lin, value %inr% c(-1, .5))
+  where(lin, value %inr% c(-1, .5), "range")
+  
+  a <- 1
+  where(lin, value > a, "first")
+  where(lin, value < a, "last")
+  
+  where(lin, value > 2, "any")
+  
+  set.seed(4353)
+  plot(f <- rgp(5, 11L), pch = as.character(1:5), points = TRUE)
+  where(f, value == max(value))
+}
---
+++ tests/testthat/test-where.R
@@ -0,0 +1,16 @@
+context("where")
+test_that("where works", {
+  lin <- 1:2 * tfd(seq(-1, 1,l = 11), seq(-1, 1, l = 11))
+  expect_identical(where(lin, value %inr% c(-1, -.5)), 
+    list(c(-1.0, -0.8, -0.6), c(-0.4)))
+  expect_identical(where(lin, value > 0, "first"),
+    list(c(0.2), c(0.2)))
+  expect_identical(where(lin, value < 0, "last"),
+    list(c(-0.2), c(-0.2)))
+  expect_identical(where(lin, value <= 0, "range"),
+    list(c(-1, 0), c(-1, 0)))
+  expect_identical(where(lin, value < -1.5, "any"),
+    c(FALSE, TRUE))
+  expect_identical(where(lin, value < -2),
+    list(numeric(0), numeric(0)))
+})
