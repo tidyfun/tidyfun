78139703162f55d675ffbbeb2be7bf09d27f77f7
fabian-s <fabian.scheipl@googlemail.com>
2018-07-30 16:31:15 -0700

    adresses #30, still to do: flesh out, link help pages

---
+++ R/tidyfun-package.r
@@ -0,0 +1,19 @@
+#' tidyfun
+#'
+#'`tidyfun` makes data wrangling and exploratory analysis for functional data 
+#' in `R` easier.\cr\cr
+#' `tidyfun` provides:  
+#'   
+#'   - new data types for representing functional data: [tfd()] & [tfb()]
+#'   - arithmetic operators ([Ops.tf()]),
+#'   - descriptive statistics: e.g. [mean.tf()], [median.tf()] 
+#'   - and graphics functions for such data: [plot.tf()], [geom_spaghetti()], [gglasagna()]
+#'   - functions to do smoothing ([smooth.tfd()]), differentiation and integration ([deriv.tfd()])
+#' - `tidyverse`-verbs for handling functional data inside data frames: [tf_gather()] etc.  
+#' \cr\cr
+#' \cr
+#' Also see `vignette("Introducing tidyfun", "tidyfun")` for a brief introduction.
+#'
+#' @name tidyfun
+#' @docType package
+NULL
