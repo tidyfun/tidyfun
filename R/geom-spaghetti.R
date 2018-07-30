2c7745ace550d74e26904f92a986af427e89ceae
fabian-s <fabian.scheipl@googlemail.com>
2018-07-30 14:32:26 -0700

    clean up geom_spaghetti. closes #51, adresses #39

---
+++ R/geom-spaghetti.R
@@ -0,0 +1,137 @@
+#' Spaghetti plots for `tf` objects
+#' 
+#' Plots a line for each entry of a `tf`-object.  
+#' `geom_spaghetti` does spaghetti plots, `geom_meatballs` does spaghetti plots
+#' with points for the actual evaluations.
+#' 
+#' @section `y` aesthetic:
+#'   Mandatory. Used to designate a column of class `tf` to be visualized. 
+#' @examples
+#' set.seed(1221)
+#' data = data.frame(col = sample(gl(5, 2)))
+#' data$f = rgp(10)
+#' data$fi = jiggle(data$f)
+#' data$fb = tfb(data$f)
+#' library(ggplot2)
+#' ggplot(data, aes(y = f, color = depth(f))) + geom_spaghetti()
+#' ggplot(data, aes(y = fi, shape = col, color = col)) + geom_meatballs()
+#' ggplot(data, aes(y = fi)) + geom_meatballs(spaghetti = FALSE) + 
+#'   facet_wrap(~col)
+#' @name ggspaghetti
+NULL
+
+# FIXME
+# > ggplot(data, aes(y = f, color = depth(f))) + geom_spaghetti() +
+#   + annotate("text", x = 1.05, y = runif(10), label = 1:10)
+# Error: evaluation nested too deeply: infinite recursion / options(expressions=)?
+#   Error during wrapup: evaluation nested too deeply: infinite recursion / options(expressions=)?
+
+is.finite.tf <- function(x) map(evaluations(x), ~ all(is.finite(x) | !is.na(x)))
+scale_type.tf <- function(x) "identity"
+
+#' @export
+#' @importFrom ggplot2 ggproto Stat Geom
+#' @rdname ggspaghetti
+#' @usage NULL
+#' @format NULL
+StatTf <- ggproto("StatTf", Stat,
+  required_aes = "y",
+  setup_params = function(data, params) {
+    if (is.null(params$arg))
+      params$arg <- list(arg(pull(data, y)))
+    params
+  },
+  compute_layer = function(self, data, params, layout) {
+    stopifnot(is_tf(pull(data, y)))
+    tf_eval <- 
+      suppressMessages(tf_unnest(data, y, .arg = params$arg, .sep = "___")) %>%
+      select(-group) %>% 
+      rename(group = y___id, x = y___arg, y = y___value)
+    tf_eval
+  },
+  # need this so arg, order_by gets recognized as valid parameters
+  # because layer() only checks compute_panel & compute_group
+  compute_panel = function(self, data, scales, arg, spaghetti) {
+    Stat$compute_panel(self, data, scales)
+  }
+)
+
+#' @export
+#' @rdname ggspaghetti
+#' @inheritParams ggplot2::stat_identity
+#' @param na.rm remove NAs? defaults to `TRUE`
+stat_tf <- function(mapping = NULL, data = NULL, geom = "spaghetti",
+  position = "identity", na.rm = TRUE, show.legend = NA, 
+  inherit.aes = TRUE, arg = NULL, ...) {
+  layer(
+    stat = StatTf, data = data, mapping = mapping, geom = geom, 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, arg = arg, ...)
+  )
+}
+
+# geom --------------------------------------------------------------------
+
+#' @export
+#' @rdname ggspaghetti
+#' @format NULL
+#' @param arg where to evaluate `tf` -- defaults to the default ;)
+geom_spaghetti <- function(mapping = NULL, data = NULL,
+  position = "identity", na.rm = TRUE, show.legend = NA, 
+  inherit.aes = TRUE, arg = NULL, ...) {
+  layer(
+    stat = StatTf, data = data, mapping = mapping, geom = "spaghetti", 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, arg = arg, ...)
+  )
+}
+#' @export
+#' @rdname ggspaghetti
+#' @usage NULL
+#' @format NULL
+GeomSpaghetti <- ggproto("GeomSpaghetti", Geom,
+  setup_data = function(data, params) {
+    GeomLine$setup_data(data, params)
+  },
+  draw_group = function(data, panel_params, coord) {
+    GeomLine$draw_panel(data, panel_params, coord)
+  },
+  default_aes = aes(colour = "black", size = 0.5,
+    linetype = 1, alpha = 0.5),
+  draw_key = GeomLine$draw_key,
+  required_aes = c("y")
+)
+
+
+#' @export
+#' @rdname ggspaghetti
+#' @format NULL
+#' @importFrom grid gList
+#' @param spaghetti plot noodles along with meatballs? defaults to true.
+geom_meatballs <- function(mapping = NULL, data = NULL,
+  position = "identity", na.rm = TRUE, show.legend = NA, 
+  inherit.aes = TRUE, arg = NULL, spaghetti = TRUE, ...) {
+  layer(
+    stat = StatTf, data = data, mapping = mapping, geom = "meatballs", 
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, arg = arg, spaghetti = spaghetti, ...)
+  )
+}
+#' @export
+#' @rdname ggspaghetti
+#' @usage NULL
+#' @format NULL
+GeomMeatballs <- ggproto("GeomMeatball", Geom,
+  setup_data = function(data, params) {
+    GeomLine$setup_data(data, params)
+  },
+  draw_group = function(data, panel_params, coord, spaghetti = TRUE) {
+    grid::gList(
+      if (spaghetti) GeomLine$draw_panel(data, panel_params, coord),
+      GeomPoint$draw_panel(data, panel_params, coord))
+  },
+  default_aes = aes(colour = "black", size = 0.5,
+    linetype = 1, alpha = 0.5, shape = 19, fill = NA, stroke = 0.5),
+  draw_key = GeomLine$draw_key,
+  required_aes = c("y")
+)
--- inst/dev/ggplot-dev.R
+++ inst/dev/ggplot-dev.R
@@ -1,123 +1,45 @@
-# cf. https://github.com/tidyverse/ggplot2/blob/69dfc4bc62828ecc3d1cda3dea4d0a664c4ea12c/R/sf.R
-rm(list = ls())
-devtools::load_all("~/fda/tidyfun")
-is.finite.tfd <- function(x) map(evaluations(x), ~ all(is.finite(x)))
-scale_type.tf <- function(x) "identity"
-library(magrittr)
-library(tidyverse)
+library(devtools)
+library(checkmate)
+library(dplyr)
+library(purrr)
+library(scales)
+library(ggplot2)
 
-#' @importFrom ggplot2 ggproto Stat Geom 
-StatTfd <- ggproto("StatTfd", Stat,
-  required_aes = "tfd",
-  default_aes = aes(x = stat(.arg), y = stat(.value), group = stat(.id), 
-    order = NULL),
-  setup_params = function(data, params) {
-    if (!is.null(params$arg))
-      return(params)
-    params$arg <- arg(pull(data, tfd))
-    if (!is.null(params$order_by)) 
-      stopifnot(is.function(params$order_by))
-    params
-  },
-  compute_layer = function(self, data, params, layout) {
-    tfd_eval <- evaluate(object = data, arg = params$arg, tfd) %>%
-      select(-group) %>% 
-      unnest(.id = ".id") %>% 
-      rename(.arg = arg, .value = data) 
-    if (is.null(data$order) & is.null(params$order_by)) {
-      ordered_id <- tfd_eval %>% pull(.id) %>% unique
-    } 
-    if (is.null(data$order) & !is.null(params$order_by)) {
-      ordered_id <- tfd_eval %>% 
-        group_by(.id) %>% 
-        summarize(order = params$order_by(.value)) %>%
-        arrange(order) %>% pull(.id)
-    } 
-    if (!is.null(data$order) & is.null(params$order_by)) {
-      ordered_id <- tfd_eval %>% arrange(order) %>% pull(.id) %>% unique
-    }
-    if (!is.null(data$order) & !is.null(params$order_by)) {
-      ordered_id <- tfd_eval %>% 
-        group_by(.id) %>% 
-        summarize(
-          order2 = ordered(params$order_by(.value)), 
-          order = order[1]) %>% 
-        arrange(order, order2) %>% pull(.id) %>% unique
-    }
-    tfd_eval <- mutate(tfd_eval, .id = ordered(.id, ordered_id))
-    tfd_eval
-  },
-  # need this so arg, order_by gets recognized as valid parameters
-  # because layer() only checks compute_panel & compute_group
-  compute_panel = function(self, data, scales, arg, order_by) {
-    browser()
-    Stat$compute_panel(self, data, scales)
-  }  
-)
-stat_tfd <- function(mapping = NULL, data = NULL, geom = "line",
-  position = "identity", na.rm = FALSE, show.legend = NA, 
-  inherit.aes = TRUE, arg = NULL, ...) {
-  layer(
-    stat = StatTfd, data = data, mapping = mapping, geom = geom, 
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, arg = arg, ...)
-  )
-}
+load_all(".")
 
-geom_spaghetti <- function(mapping = NULL, data = NULL,
-  position = "identity", na.rm = FALSE, show.legend = NA, 
-  inherit.aes = TRUE, arg = NULL, ...) {
-  layer(
-    stat = StatTfd, data = data, mapping = mapping, geom = "line", 
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, arg = arg, ...)
-  )
-}
-geom_meatballs <- function(mapping = NULL, data = NULL,
-  position = "identity", na.rm = FALSE, show.legend = NA, 
-  inherit.aes = TRUE, arg = NULL, spaghetti = TRUE, ...) {
-  list(
-    layer(
-    stat = StatTfd, data = data, mapping = mapping, geom = "point", 
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, arg = arg, ...)),
-    if (spaghetti) {
-      layer(
-        stat = StatTfd, data = data, mapping = mapping, geom = "line", 
-        position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-        params = list(na.rm = na.rm, arg = arg, ...))
-    } else NULL) 
-}
-geom_lasagna <- function(mapping = list(), 
-  data = NULL,  position = "identity", na.rm = FALSE, show.legend = NA, 
-  inherit.aes = TRUE, arg = NULL, order_by = NULL, size = 4, ...) {
-  default_mapping <- 
-    aes(x = stat(.arg), y = stat(.id), colour = stat(.value))
-  mapping <- structure(modifyList(mapping, default_mapping), class = "uneval")
-  layer(
-    stat = StatTfd, data = data, mapping = mapping, geom = "line",
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, arg = arg, size = size, 
-      order_by = order_by)
-  ) 
-}
 
 set.seed(1221)
-data = data.frame(col = sample(gl(5, 1)))
-data$f = rgp(5)
+data = data.frame(col = sample(gl(5, 2)))
+data$f = rgp(10); names(data$f) <- letters[1:10]
 data$fi = jiggle(data$f)
 data$fb = tfb(data$f)
 
-ggplot(data, aes(tfd = f, color = depth(f))) + geom_spaghetti() + 
+load_all()
+gglasagna(data, f, order = col)
+
+tf_eval <- 
+  suppressMessages(tf_unnest(data, f, .sep = "___")) %>%
+  rename(y = f___id, x = f___arg, fill = f___value)
+if (is.null(tf_eval$label)) tf_eval <- mutate(tf_eval, label = y)
+ordered <- tf_eval %>%  select(y, label) %>% group_by(y) %>% slice(1)
+tf_eval <- tf_eval %>% mutate(y = ordered(y, levels = pull(ordered, y), 
+  labels = pull(ordered, label)))
+p <- ggplot(tf_eval) + geom_tile(aes(y = y, x = x, fill = fill))
+q <- ggplot(data) + geom_lasagna(aes(y  = names(f), fill = f))
+
+
+
+ggplot(data, aes(y = f, color = depth(f))) + geom_spaghetti() + 
   annotate("text", x = 1.05, y = drop(data$f[, 1]), label = 1:nrow(data))
-ggplot(data, aes(tfd = f, shape = col, color = col)) + geom_meatballs()
-ggplot(data, aes(tfd = f)) + geom_meatballs(spaghetti = FALSE) + 
+ggplot(data, aes(y = f, shape = col, color = col)) + geom_meatballs()
+ggplot(data, aes(y = f)) + geom_meatballs(spaghetti = FALSE) + 
   facet_wrap(~col)
 
 # geom_lasagna is a hack of geom_line because geom_tile won't accept the 
 # id-factor as a vertical axis -- adjust line width ("size") to get a proper 
 # lasagna without gaps just like nonna used to make back in the old country:
-ggplot(data, aes(tfd = fb, order = col)) + geom_lasagna(size = 8)
+load_all(".")
+ggplot(data, aes(fill = fb, order = col)) + geom_lasagna()
 ggplot(data, aes(tfd = fb)) + geom_lasagna() + facet_wrap(~ col, scales = "free")
 
 # use the order-aesthetic to define the vertical ordering of the functions 
@@ -135,7 +57,7 @@ ggplot(data, aes(tfd = f, order = drop(f[, 0]))) + geom_lasagna()
 ggplot(data, aes(tfd = f, order = col)) + geom_lasagna(order_by = function(f) f[1]) + 
   facet_wrap(col ~ ., scales = "free")
 
-
+gglasagna(data, fb, order = "col")
 
 # look at GeomSf: https://github.com/tidyverse/ggplot2/blob/17b45f906c3a55d187915f4f3010836a804051ae/R/sf.R
 
