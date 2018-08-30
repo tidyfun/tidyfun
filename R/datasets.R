c2125290d9c7b80386f42909f37515dfb528929a
fabian-s <fabian.scheipl@googlemail.com>
2018-08-30 17:18:29 +0200

    document chf data

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+# Documentation for tidyfun datasets
+
+#' @name chf_df
+#' @docType data
+#' @title Congestive heart failure data: 
+#' @description Activity data from a study of congestive heart failure. 
+#' Data were originally presented
+#' in "Multilevel Matrix-Variate Analysis and its Application 
+#' to Accelerometry-Measured Physical Activity in Clinical 
+#' Populations" by Huang et al.; these data are public,
+#' with download information in the paper. 
+#' @keywords data
+NULL
