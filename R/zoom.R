f756bdbbea6e5ba905323b10f91a6194a5a43046
fabian-s <fabian.scheipl@googlemail.com>
2018-08-30 22:52:53 +0200

    #54: rename zoom -> tf_zoom

--- R/zoom.R
+++ R/zoom.R
@@ -15,14 +15,14 @@
 #' @examples 
 #'   (x <- rgp(10))
 #'   plot(x)
-#'   zoom(x, .5, .9)
-#'   lines(zoom(x, .5, .9), col = "red")
-#'   points(zoom(x, seq(0, .5, l = 10), seq(.5, 1, l = 10)), col = "blue")
-zoom <- function(f, begin, end, ...) {
-  UseMethod("zoom")
+#'   tf_zoom(x, .5, .9)
+#'   lines(tf_zoom(x, .5, .9), col = "red")
+#'   points(tf_zoom(x, seq(0, .5, l = 10), seq(.5, 1, l = 10)), col = "blue")
+tf_zoom <- function(f, begin, end, ...) {
+  UseMethod("tf_zoom")
 }
 
-prep_zoom_args <- function(f, begin, end) {
+prep_tf_zoom_args <- function(f, begin, end) {
   assert_numeric(begin, any.missing = FALSE, min.len = 1, max.len = length(f))
   assert_numeric(end, any.missing = FALSE, min.len = 1, max.len = length(f))
   regular <- TRUE
@@ -38,17 +38,17 @@ prep_zoom_args <- function(f, begin, end) {
   list(begin = begin, end = end, dom = new_domain, regular = regular)
 }
 
-#' @rdname zoom
+#' @rdname tf_zoom
 #' @export
-zoom.tfd <- function(f, begin = domain(f)[1], end = domain(f)[2], ...) {
-  args <- prep_zoom_args(f, begin, end)
+tf_zoom.tfd <- function(f, begin = domain(f)[1], end = domain(f)[2], ...) {
+  args <- prep_tf_zoom_args(f, begin, end)
   ret <- pmap(list(f[ , arg(f), matrix = FALSE], args$begin, args$end), 
     ~ filter(..1, arg >= ..2 & arg <= ..3))
   ret <- tfd(ret, domain = args$dom, resolution = attr(f, "resolution"))
   if (is_irreg(ret)) {
     nas <- map_lgl(ret, ~ length(.x$arg) == 0)
     if (all(nas)) stop("no data in zoom region.")
-    if (any(nas)) warning("NAs created by zoom.")
+    if (any(nas)) warning("NAs created by tf_zoom.")
     for (n in which(nas)) ret[[n]] <- list(arg = unname(args$dom[1]), value = NA)
   } else {
     if (any(map_lgl(ret, ~ length(.x) == 0))) 
@@ -57,13 +57,13 @@ zoom.tfd <- function(f, begin = domain(f)[1], end = domain(f)[2], ...) {
   evaluator(ret) <- attr(f, "evaluator_name")
   ret
 }
-#' @rdname zoom
+#' @rdname tf_zoom
 #' @export
-zoom.tfb <- function(f, begin = domain(f)[1], end = domain(f)[2], ...) {
-  args <- prep_zoom_args(f, begin, end)
+tf_zoom.tfb <- function(f, begin = domain(f)[1], end = domain(f)[2], ...) {
+  args <- prep_tf_zoom_args(f, begin, end)
   if (!args$regular) {
-    message("zoom() with varying start or end points - converting to tfd.")
-    return(zoom(tfd(f), begin, end))
+    message("tf_zoom() with varying start or end points - converting to tfd.")
+    return(tf_zoom(tfd(f), begin, end))
   }
   use <- arg(f) >= args$dom[1] & arg(f) <= args$dom[2]
   if (!any(use)) stop("no data in zoom region.")
--- tests/testthat/test-zoom.R
+++ tests/testthat/test-zoom.R
@@ -1,4 +1,4 @@
-context("zooming")
+context("tf_zooming")
 
 set.seed(123)
 x <- rgp(4, arg = seq(0, 1, l = 51), nugget = .1)
@@ -7,31 +7,31 @@ xb <- tfb(x, verbose = FALSE)
 xbi <- tfb_fpc(xi, nbasis = 5)
 
 
-test_that("zoom for tfd works", {
-  expect_equal(domain(zoom(x, .2, .8)), c(.2, .8))
-  expect_equal(domain(zoom(xi, .2, .8)), c(.2, .8))
-  expect_equivalent(as.matrix(zoom(x, 0, .5)), as.matrix(x)[,1:26])
-  expect_equivalent(as.data.frame(zoom(xi, 0, .5)), 
+test_that("tf_zoom for tfd works", {
+  expect_equal(domain(tf_zoom(x, .2, .8)), c(.2, .8))
+  expect_equal(domain(tf_zoom(xi, .2, .8)), c(.2, .8))
+  expect_equivalent(as.matrix(tf_zoom(x, 0, .5)), as.matrix(x)[,1:26])
+  expect_equivalent(as.data.frame(tf_zoom(xi, 0, .5)), 
     as.data.frame(xi) %>% dplyr::filter(arg <= .5))
   
-  expect_error(zoom(x, c(.8, .1)))
-  expect_error(zoom(x, .11, .111), "no data")
-  expect_error(zoom(xi, 0.051, 0.0511), "no data")
+  expect_error(tf_zoom(x, c(.8, .1)))
+  expect_error(tf_zoom(x, .11, .111), "no data")
+  expect_error(tf_zoom(xi, 0.051, 0.0511), "no data")
   
-  expect_true(is_irreg(zoom(x, .2, seq(.3, 1, l = length(x)))))
+  expect_true(is_irreg(tf_zoom(x, .2, seq(.3, 1, l = length(x)))))
 })
 
 
-test_that("zoom for tfb works", {
-  expect_equal(domain(zoom(xb, .2, .8)), c(.2, .8))
-  expect_equal(domain(zoom(xbi, .2, .8)), c(.2, .8))
-  expect_equivalent(as.matrix(zoom(xb, 0, .5)), as.matrix(xb)[,1:26])
-  expect_equivalent(as.data.frame(zoom(xbi, 0, .5)), 
+test_that("tf_zoom for tfb works", {
+  expect_equal(domain(tf_zoom(xb, .2, .8)), c(.2, .8))
+  expect_equal(domain(tf_zoom(xbi, .2, .8)), c(.2, .8))
+  expect_equivalent(as.matrix(tf_zoom(xb, 0, .5)), as.matrix(xb)[,1:26])
+  expect_equivalent(as.data.frame(tf_zoom(xbi, 0, .5)), 
     as.data.frame(xbi) %>% dplyr::filter(arg <= .5))
   
-  expect_error(zoom(xb, c(.8, .1)))
-  expect_error(zoom(xb, .11, .111), "no data")
+  expect_error(tf_zoom(xb, c(.8, .1)))
+  expect_error(tf_zoom(xb, .11, .111), "no data")
 
-  expect_message(zoom(xb, .2, seq(.3, 1, l = length(x))), "converting")
-  expect_true(is_irreg(zoom(xb, .2, seq(.3, 1, l = length(x)))))
+  expect_message(tf_zoom(xb, .2, seq(.3, 1, l = length(x))), "converting")
+  expect_true(is_irreg(tf_zoom(xb, .2, seq(.3, 1, l = length(x)))))
 })
