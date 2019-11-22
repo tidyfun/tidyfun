6e1c632ca586619d93a74609985538182d0214fa
SvenLorenz <sven.m.lorenz@gmail.com>
2019-11-23 00:04:12 +0100

    bug fix for tf_evaluations.tfb_wavelet

--- R/methods.R
+++ R/methods.R
@@ -42,7 +42,7 @@ tf_evaluations.tfb <- function(f) {
 
 #' @export
 tf_evaluations.tfb_wavelet <- function(f) {
-  evals <- map2(f, attr(f, "slope_params"), function(x, y) {
+  evals <- map2(f, attr(f, "slope_params")[1:length(f)], function(x, y) {
     func <- y[1] + y[2] * attr(f, "arg")
     X <- cbind(attr(f, "basis_matrix"), func)
     drop(X %*% x)
