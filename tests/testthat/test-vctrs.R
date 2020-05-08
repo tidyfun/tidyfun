92742bd367420406e8a6d62eeee75e6c4d0e311b
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-05-08 19:26:58 -0400

    small stuff to make vctrs tests work

--- tests/testthat/test-vctrs.R
+++ tests/testthat/test-vctrs.R
@@ -3,6 +3,7 @@ context("vctrs")
 x = tf_rgp(1, arg = seq(0, 1, l = 11))
 y = tf_rgp(1, arg = seq(0, 1, l = 21))
 
+cca = dti_df$cca
 rcst = dti_df$cca
 
 test_that("concatenation behaves for tfd", {
@@ -13,18 +14,19 @@ test_that("concatenation behaves for tfd", {
   expect_numeric(tf_evaluate(c(x, y)[1])[[1]])
 })
 
-cca = dti_df$cca
+
 tfb_k10 = cca %>% tfb(k = 10)
 tfb_k20 = cca %>% tfb(k = 20)
 z = tf_rgp(10, arg = seq(0, 1, l = 11)) %>% tfb_fpc()
 z2 = z %>% tfb_fpc(npc = 10)
 
 test_that("concatenation behaves for tfb", {
-  expect_is(c(cca_tfb[1], cca_tfb[2]), "tfb_spline")
-  expect_is(c(tfb(x), tfb(x)), "tfb_spline")
+  expect_is(c(tfb_k10[1], tfb_k10[2]), "tfb_spline")
+  expect_is(c(tfb(tfb_k10[1]), tfb(tfb_k10[2])), "tfb_spline")
   expect_is(c(z, z), "tfb_fpc")
   
   expect_warning(c(tfb_k10, tfb_k20))
   expect_error(c(tfb_k10, tfb_fpc(z)))
+  expect_error(c(tfb_fpc(z), tfb_k10))
   expect_error(c(z, z2))
 })
