f1234356f6d762f2d11ac8e32051b27811c31349
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-07-17 19:06:38 +0200

    refactor: remove duplicate ggplot2 imports since already importing package

--- R/geom-spaghetti.R
+++ R/geom-spaghetti.R
@@ -37,7 +37,6 @@ is.finite.tf <- function(x) {
 scale_type.tf <- function(x) "identity"
 
 #' @export
-#' @importFrom ggplot2 ggproto Stat Geom
 #' @importFrom dplyr pull
 #' @rdname ggspaghetti
 #' @family tidyfun visualization
