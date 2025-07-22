1c6fd7d5db9ff63cce5a3da40215d012628ce23f
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-07-22 23:28:46 +0200

    docs: modernise data docs

--- R/datasets.R
+++ R/datasets.R
@@ -1,32 +1,40 @@
 # Documentation for tidyfun datasets
 
-#' @name chf_df
-#' @docType data
-#' @title Congestive heart failure accelerometry data
-#' @description Activity data from a study of congestive heart failure.
-#' Data were originally presented
-#' in "Multilevel Matrix-Variate Analysis and its Application
-#' to Accelerometry-Measured Physical Activity in Clinical
-#' Populations" by Huang et al.; these data are public,
-#' with download information in the paper.
-#' @keywords data
-#' @family tidyfun datasets
-NULL
-
-
-#' @name dti_df
-#' @docType data
-#' @title Diffusion tensor imaging data
-#' @description Fractional anisotropy (FA) tract profiles for the corpus
-#' callosum (cca) and the right corticospinal tract (rcst). Accompanying
-#' the tract profiles are the subject ID numbers, visit number, total
-#' number of scans, multiple sclerosis case status and Paced Auditory Serial
-#' Addition Test (pasat) score.
+#' Congestive heart failure accelerometry data
 #'
-#' Data are also include in the `refund` package in another format.
-#' @details If you use this data as an example in written work, please
-#' include the following acknowledgment: “The MRI/DTI data were collected
-#' at Johns Hopkins University and the Kennedy-Krieger Institute"
+#' @description
+#' Activity data from a study of congestive heart failure. Data
+#' were originally presented in "Multilevel Matrix-Variate Analysis
+#' and its Application to Accelerometry-Measured Physical Activity
+#' in Clinical Populations" by Huang et al.; these data are public,
+#' with download information in the paper.
+#'
 #' @keywords data
 #' @family tidyfun datasets
-NULL
+#' @examples
+#' chf_df
+"chf_df"
+
+#' Diffusion tensor imaging data
+#'
+#' @description
+#' Fractional anisotropy (FA) tract profiles for the corpus
+#' callosum (cca) and the right corticospinal tract (rcst).
+#' Accompanying the tract profiles are the subject ID numbers,
+#' visit number, total number of scans, multiple sclerosis case
+#' status and Paced Auditory Serial Addition Test (pasat) score.
+#'
+#' Data are also include in the \CRANpkg{refund} package in another
+#' format.
+#'
+#' @details
+#' If you use this data as an example in written work, please
+#' include the following acknowledgment: "The MRI/DTI data were
+#' collected at Johns Hopkins University and the Kennedy-Krieger
+#' Institute"
+#'
+#' @keywords data
+#' @family tidyfun datasets
+#' @examples
+#' dti_df
+"dti_df"
